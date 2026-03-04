5e0638a3497779d5145b794cd0558a9dadd46f37
DrSRasin <fuhrrme@biu.ac.il>
2026-03-04 18:50:11 -0500

    Task 7: Error handling for blank input

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,23 +1,25 @@
-import requests
-import json
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
 
-def emotion_detector(text_to_analyze):
-    """Detect emotions in text using Watson NLP API."""
-    url = ('https://sn-watson-emotion.labs.skills.network/v1/'
-           'watson.runtime.nlp.v1/NlpService/EmotionPredict')
-    headers = {
-        'grpc-metadata-mm-model-id': 'emotion_aggregated-workflow_lang_en_stock'
-    }
-    myobj = {"raw_document": {"text": text_to_analyze}}
-    response = requests.post(url, json=myobj, headers=headers)
-    formatted_response = json.loads(response.text)
-    emotions = formatted_response['emotionPredictions'][0]['emotion']
-    dominant_emotion = max(emotions, key=emotions.get)
-    return {
-        'anger': emotions['anger'],
-        'disgust': emotions['disgust'],
-        'fear': emotions['fear'],
-        'joy': emotions['joy'],
-        'sadness': emotions['sadness'],
-        'dominant_emotion': dominant_emotion
-    }
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def sent_analyzer():
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = emotion_detector(text_to_analyze)
+    if response['dominant_emotion'] is None:
+        return "Invalid text! Please try again!"
+    return (f"For the given statement, the system response is "
+            f"'anger': {response['anger']}, "
+            f"'disgust': {response['disgust']}, "
+            f"'fear': {response['fear']}, "
+            f"'joy': {response['joy']} and "
+            f"'sadness': {response['sadness']}. "
+            f"The dominant emotion is {response['dominant_emotion']}.")
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
